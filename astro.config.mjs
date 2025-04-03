d781b525b803d7be11cde3dd71c1486afc9424bc
Alizia Kaline <roimichael70@hotmail.fr>
2025-04-03 23:06:35 +0200

    Change site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from "@astrojs/starlight";
 	
 // https://astro.build/config
 export default defineConfig({
-	site: "https://meiyo40.github.cio",
+	site: "https://renegadebananas.github.io",
 	base: "/",
 	integrations: [
 		starlight({
